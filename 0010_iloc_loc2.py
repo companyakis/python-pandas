61eeda68baee8c007267cb47d8ad062bfd8b48f1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-21 23:12:15 +0300

    Create 0010_iloc_loc2.py

---
+++ 0010_iloc_loc2.py
@@ -0,0 +1,15 @@
+print(df.iloc[0, 3]) # 37
+
+print(df.iloc[8, 0]) # 34
+
+print(df.iloc[5,2] == 34) # False
+
+print(df.iloc[:3, 1:3])
+
+"""
+   quarter2  quarter3
+0        33        39
+1        18        41
+2         3        46
+
+"""
